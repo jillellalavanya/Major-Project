b31b479c933eb53b3d98af83932ac64f26f03aff
jillellalavanya <84625452+jillellalavanya@users.noreply.github.com>
2021-06-09 20:52:22 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import joblib
+model = joblib.load('sentiment-analysis')
+st.title('sentiment-analysis')
+ip = st.text_input("Enter the message")
+op = model.predict([ip])
+if st.button('Predict'):
+  st.title(op[0]) 
+  
